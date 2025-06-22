a76aa4e158361405d844516051c7da0d6dc9173e
Alpha-Clifty <kc_majic@hotmail.com>
2025-06-22 21:45:09 +0100

    added num_words

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    split_words = text.split()
+    num_words = 0
+    for word in split_words:
+        num_words += 1
+    print(f"{num_words} words found in ther document")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
